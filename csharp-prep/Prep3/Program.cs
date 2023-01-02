2205264b5cb890638ce529a9e3ea3c805b13d8e1
ansoncolam <lam22015@byui.edu>
2023-01-02 22:10:15 +0800

    guessing number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool loopSwitch = false;
+        int guesses = 0;
+
+        Random randomGenerator = new Random();
+        int magicNumberInt = randomGenerator.Next(1, 100);
+
+        while (loopSwitch == false) {
+            guesses += 1;
+            Console.Write("What is your guess? ");
+            string guessNumber = Console.ReadLine();
+            int guessNumberInt = int.Parse(guessNumber);
+
+            if (magicNumberInt > guessNumberInt) {
+                Console.WriteLine("Higher");
+            } else if (magicNumberInt < guessNumberInt) {
+                Console.WriteLine("Lower");
+            } else if (magicNumberInt == guessNumberInt) {
+                Console.WriteLine("You guessed it!");
+                if (guesses == 1) {
+                    Console.WriteLine("You has made 1 guess only to get the answer!");
+                } else {
+                    Console.WriteLine($"You has made {guesses} guesses to get the answer!");
+                }
+                Console.WriteLine("Do you want to play again? ");
+                string answer = Console.ReadLine();
+                if (answer.ToLower() == "yes") {
+                    magicNumberInt = randomGenerator.Next(1, 100);
+                    guesses = 0;
+                } else {
+                    Console.WriteLine("Thanks for playing!");
+                    loopSwitch = true;
+                }
+            }
+        }
+
     }
 }

4a9e52982265c814977751c32c19c7863da178fe
ansoncolam <lam22015@byui.edu>
2023-01-07 13:32:54 +0800

    updated files

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+
+        Job job2 = new Job();
+        // job2._jobTitle="Software ";
+        job2._company = "Apple";
+
+        Console.WriteLine(job1._company);
+        Console.WriteLine(job2._company);
     }
 }

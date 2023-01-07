a2872d18ca73a8bbc59cb6632a42f2f135cb3cb1
ansoncolam <lam22015@byui.edu>
2023-01-07 15:28:57 +0800

    updated step 8

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Resume {
     public string _name;
-    public List<Job> _jobs;
+    public List<Job> _jobs = new List<Job>();
 
     public void Display() {
         Console.WriteLine($"Name: {_name}");
